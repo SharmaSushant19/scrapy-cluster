11fbf63e4f288d8891f17d171b10cb6a3213097b
Sushant Sharma <mailtosushant19@gmail.com>
2018-04-06 12:55:51 +0530

    Add files via upload
    This script maintains the feeding of urls to the crawler

---
+++ kafka-monitor/script.py
@@ -0,0 +1,19 @@
+import os
+import time
+import subprocess
+import zc.lockfile
+import sys
+while(1):
+        try:
+                print("trying to acquire a lock on the file")
+                lock = zc.lockfile.LockFile("lock.txt")
+                print("lock acquired")
+                #t = os.system(temp)
+                t = subprocess.call(['python','feedurl.py'])
+                print(t)
+        except zc.lockfile.LockError:
+                print("not able to lock the file_going to sleep")
+                time.sleep(600)
+        #the script completed successfully
+        else:
+                lock.close()
